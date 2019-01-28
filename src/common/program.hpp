d8e21fd7b37c4c2e005cf893d9af79913d00c0be
Ivo van Dongen <info@ivovandongen.nl>
2019-01-28 16:08:02 +0200

    lesson03 - uniforms in program abstraction

--- src/common/program.hpp
+++ src/common/program.hpp
@@ -17,6 +17,30 @@ public:
 
     void unbind();
 
+    void inline setUniform(const std::string &name, bool value) const {
+        glUniform1i(glGetUniformLocation(_id, name.c_str()), (int) value);
+    }
+
+    void inline setUniform(const std::string &name, int value) const {
+        glUniform1i(glGetUniformLocation(_id, name.c_str()), value);
+    }
+
+    void inline setUniform(const std::string &name, float value) const {
+        glUniform1f(glGetUniformLocation(_id, name.c_str()), value);
+    }
+
+    void inline setUniform(const std::string &name, float x, float y) const {
+        glUniform2f(glGetUniformLocation(_id, name.c_str()), x, y);
+    }
+
+    void inline setUniform(const std::string &name, float x, float y, float z) const {
+        glUniform3f(glGetUniformLocation(_id, name.c_str()), x, y, z);
+    }
+
+    void inline setUniform(const std::string &name, float x, float y, float z, float w) const {
+        glUniform4f(glGetUniformLocation(_id, name.c_str()), x, y, z, w);
+    }
+
 private:
     GLuint _id;
 };
