530b99843c29f5402635b969c2242436f35d758e
Ivo van Dongen <info@ivovandongen.nl>
2019-02-04 12:13:01 +0200

    lesson15 - visualizing depth buffer linearly

--- src/lesson15_depth_buffer/depth_testing.fragment.glsl
+++ src/lesson15_depth_buffer/depth_testing.fragment.glsl
@@ -6,6 +6,15 @@ in vec2 texCoords;
 
 uniform sampler2D texture1;
 
+float near = 0.1;
+float far  = 100.0;
+
+float LinearizeDepth(float depth) {
+    float z = depth * 2.0 - 1.0; // back to NDC
+    return (2.0 * near * far) / (far + near - z * (far - near));
+}
+
 void main() {
-    fragColor = vec4(vec3(gl_FragCoord.z), 1.0);
+    float depth = LinearizeDepth(gl_FragCoord.z) / far; // divide by far for demonstration
+    fragColor = vec4(vec3(depth), 1.0);
 }
