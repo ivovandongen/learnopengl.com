2156f88742ba1ae335c30b8f005f13c56ca52a60
Ivo van Dongen <info@ivovandongen.nl>
2019-02-06 14:42:05 +0200

    lesson19 - postprocessing - weighted grayscale

--- src/lesson19_framebuffer/gs.fragment.glsl
+++ src/lesson19_framebuffer/gs.fragment.glsl
@@ -8,6 +8,6 @@ uniform sampler2D texture1;
 
 void main() {
     fragColor = texture(texture1, texCoords);
-    float average = (fragColor.r + fragColor.g + fragColor.b) / 3.0;
+    float average = 0.2126 * fragColor.r + 0.7152 * fragColor.g + 0.0722 * fragColor.b;
     fragColor = vec4(average, average, average, 1.0);
 }
